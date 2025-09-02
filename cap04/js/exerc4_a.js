63b449208a43e4cf9c128d5ace63faf2b17a5010
oarthurma <oarthurma@gmail.com>
2025-09-02 14:35:23 -0300

    ex4a

--- cap04/js/exerc4_a.js
+++ cap04/js/exerc4_a.js
@@ -1,3 +1,23 @@
 /*
   a) Elaborar um programa qeu leia um número. Informe se ele é par ou ímpar. Faça com o if... else... tradicional e, após, tente criar a condição com o operador ternário. 
 */
+const frm = document.querySelector("form") // obtém elementos da página
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault()  // evita envio do form
+  const num = Number(frm.inNum.value) // obtém número digitado
+  
+  
+  // if(num % 2 === 0) {
+  //   resultado = "par"
+  // }
+  // else {
+  //   resultado = "ímpar"
+  // }  
+
+  //ternário
+  num % 2 === 0 ? resultado = 'par' : resultado = 'impar'
+
+  resp.innerText = `${num} é ${resultado}`
+})
