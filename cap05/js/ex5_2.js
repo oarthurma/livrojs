db78b3c785bf84c280784dc55cd6978110adccad
oarthurma <oarthurma@gmail.com>
2025-10-01 14:21:21 -0300

    exemplo 5.2

--- cap05/js/ex5_2.js
+++ cap05/js/ex5_2.js
@@ -10,8 +10,12 @@ frm.addEventListener("submit", (e) => {
   for(let i = numero; i > 0; i = i - 1) {  // cria um for decrescente
     resposta = resposta + i + ", "   // resposta acumula números (e vírgulas)
   }
+
+  Nesse caso uma vírgula ficou “sobrando” no final da listagem, o que faz com que a resposta do programa não fique muito bonita.
 */
 
+/*
+ 
   for(let i = numero; i > 0; i--) {
     if(i == 1) {
       resposta = resposta + i + "." // ou resposta = `${resposta}${i}.`
@@ -20,5 +24,15 @@ frm.addEventListener("submit", (e) => {
       resposta = resposta + i + ", " // ou resposta = ´${resposta}${i},´
     }
   }
-  resp.innerText = resposta     // exibe a resposta
+
+  Para esse exercício, tal solução não é a recomendada, pois o número de comparações realizadas pelo programa é muito alto, e a solução, pouco eficiente. Imagine se o usuário digitar o número 100: o programa vai realizar 100 comparações, sendo que, em apenas uma delas, o if é verdadeiro.  
+*/
+
+for (let i = numero; i > 1; i--) {
+  resposta = resposta + 1 + ", "
+}
+  resp.innerText = resposta + "1."     // exibe a resposta
+
+  // Isolar o último número é uma alternativa mais adequada para esse programa. Ou seja, o comando for repete até o número 2 e fora da repetição é acrescentado o último número: “1.”.
+
 })
