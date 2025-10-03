2f20692fb4c115b5cef9835994f904241f98b413
oarthurma <oarthurma@gmail.com>
2025-10-03 14:48:21 -0300

    finalização 5.3

--- cap05/js/ex5_2.js
+++ cap05/js/ex5_2.js
@@ -15,7 +15,8 @@ frm.addEventListener("submit", (e) => {
 */
 
 /*
- 
+ a) criar uma condição para verificar se o número da repetição é o último a ser exibido:
+
   for(let i = numero; i > 0; i--) {
     if(i == 1) {
       resposta = resposta + i + "." // ou resposta = `${resposta}${i}.`
@@ -28,11 +29,33 @@ frm.addEventListener("submit", (e) => {
   Para esse exercício, tal solução não é a recomendada, pois o número de comparações realizadas pelo programa é muito alto, e a solução, pouco eficiente. Imagine se o usuário digitar o número 100: o programa vai realizar 100 comparações, sendo que, em apenas uma delas, o if é verdadeiro.  
 */
 
+// b) isolar o último número:
+ 
 for (let i = numero; i > 1; i--) {
-  resposta = resposta + 1 + ", "
+  resposta = resposta + i + ", "
 }
   resp.innerText = resposta + "1."     // exibe a resposta
 
   // Isolar o último número é uma alternativa mais adequada para esse programa. Ou seja, o comando for repete até o número 2 e fora da repetição é acrescentado o último número: “1.”.
 
+/*
+c) isolar o primeiro número:
+
+let resposta = `Entre ${numero} e 1: ${numero}` // já contém o 1º número
+for(let i = numero - 1; i > 0 ; i--) {
+  resposta = resposta + ", " + i
+}
+
+resp.innerText = resposta + "."     // exibe a resposta
+*/
+
+
+// Utilizando o While:
+// let i = numero      // declara e inicializa a variável i
+// while(i > 0) {     // enquanto i maior que 0
+//   resposta = resposta + i + ", "    // acumula valores de i
+//   i--     // decrementa o i (idem a i = i -1)
+// }
+//  resp.innerText = resposta + "."     // exibe a resposta
 })
+
