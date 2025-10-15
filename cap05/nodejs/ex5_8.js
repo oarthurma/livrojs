0efb37fd2295b9264f6492d237fb5814e58ca2b6
oarthurma <oarthurma@gmail.com>
2025-10-15 14:29:58 -0300

    conclusao do exemplo 5.7

---
+++ cap05/nodejs/ex5_8.js
@@ -0,0 +1,18 @@
+/*
+  a) A Copa do Mundo ocorre de 4 em 4 anos, desde 1930, exceto nos anos de 1942 e 1946 (Segunda Guerra Mundial).
+  Construir um programa que repita a leitura de números (anos) até ser digitado 0. Informe para cada ano se ele é ou não o ano de Copa do Mundo.
+*/
+
+const prompt = require("prompt-sync")() // adiciona pacote prompt-sync
+console.log("Programa Anos de Copa do Mundo. Digite 0 para sair")
+console.log("-----------------------------------")
+do {
+  const ano = Number(prompt("Ano: "))
+  if(ano == 0) {
+    break // sai de repetição
+  } else if(ano == 1942 || ano == 1946) {
+    console.log(`Não houve Copa em ${ano} (Segunda Guerra do Mundo)`)
+  } else if (ano >= 1930 && ano % 4 == 2) {
+    console.log(`Sim! ${ano} é ano de Copa do Mundo!`)
+  }
+}
